8331b6a4f73323e24a4122b8a21be7b247f6774d
ScorpionOrange <scorpionorange@126.com>
2017-02-20 00:07:03 +0800

    2017.2.19: 用Java实现网易云音乐爬虫，未完成；

--- IdeaProjects/music163crawler/src/main/java/WebPage.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ScorpionOrange on 2017/02/19.
- */
-public class WebPage {
-    public enum PageType{
-        song, playlist, playlists;
-    }
-}
---
+++ IdeaProjects/music163crawler/src/main/java/model/Song.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Created by ScorpionOrange on 2017/02/19.
+ */
+public class Song {
+    private String url;
+    private String title;
+    private long commentCount;
+}
