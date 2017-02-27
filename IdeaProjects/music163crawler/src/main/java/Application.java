ace2e58dfdc646e1df5430ffe2addd4e2d159d58
scorpionorange <scorpionorange@126.com>
2017-02-28 00:47:06 +0800

    2017.2.19: 用Java实现网易云音乐爬虫，未完成； 2017.2.20: 新增pom.xml的各种依赖及插件；

--- IdeaProjects/music163crawler/src/main/java/Application.java
+++ IdeaProjects/music163crawler/src/main/java/Application.java
@@ -2,6 +2,7 @@
  * Created by ScorpionOrange on 2017/02/20.
  */
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.logging.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,7 +15,9 @@ import java.net.URISyntaxException;
 @EnableCaching
 public class Application {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException,
+                                                  URISyntaxException,
+                                                  IllegalAccessException {
         SpringApplication.run(Application.class);
     }
 }
