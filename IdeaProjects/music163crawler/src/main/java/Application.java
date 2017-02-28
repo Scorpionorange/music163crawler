3b91cc06981e2ffda5258c3a1c2757522f37b023
scorpionorange <scorpionorange@126.com>
2017-02-28 19:15:49 +0800

    2017.2.19: 用Java实现网易云音乐爬虫，未完成； 2017.2.20: 新增pom.xml的各种依赖及插件； 2017.2.28: java.lang.IllegalArgumentException: Cannot instantiate interface     org.springframework.context.ApplicationListener :     org.springframework.boot.logging.ClasspathLoggingApplicationListener  not know how to solve it yet.

--- IdeaProjects/music163crawler/src/main/java/Application.java
+++ IdeaProjects/music163crawler/src/main/java/Application.java
@@ -2,7 +2,6 @@
  * Created by ScorpionOrange on 2017/02/20.
  */
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.logging.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -18,6 +17,11 @@ public class Application {
     public static void main(String[] args) throws IOException,
                                                   URISyntaxException,
                                                   IllegalAccessException {
-        SpringApplication.run(Application.class);
+        try{
+            SpringApplication.run(Application.class);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
